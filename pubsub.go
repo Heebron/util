d9e74ca0fea0384f07efdca2431e88a79154006b
Ken Pratt <kenpratt@comcast.net>
2025-09-04 17:25:53 -0400

    performed some cleanup

--- pubsub.go
+++ pubsub.go
@@ -24,11 +24,10 @@ func (s *PubSub[T]) Register(buffSize uint) <-chan T {
 }
 
 // Broadcast sends a message to all registered channels in the PubSub.
-// It acquires a read-lock to prevent changes to the client's slice during the broadcast.
-// The client's slice is copied into a new buffer for non-concurrent iteration.
+// It acquires a read-lock to prevent changes to the clients slice during the snapshot.
+// The clients slice is copied into a new buffer for non-concurrent iteration, then the lock is released.
 // Sends the message to each channel in the buffer.
-// Since the channels are buffered, the broadcast is non-blocking.
-// Once the broadcast is complete, the read-lock is released.
+// Note: send may block if a channel has insufficient buffer or no receiver.
 func (s *PubSub[T]) Broadcast(msg T) {
 	s.RLock()
 	buffer := make([]chan T, len(s.clients))
@@ -39,28 +38,32 @@ func (s *PubSub[T]) Broadcast(msg T) {
 	}
 }
 
-// Unregister removes the given reception channel from the PubSub. It closes the channel and removes it from the list of clients.
-// If the channel is not found, nothing happens.
+// Unregister removes the given reception channel from the PubSub. It removes it from the list of clients.
+// If the channel is not found, nothing happens. The channel is not closed by PubSub to avoid races with concurrent senders.
 func (s *PubSub[T]) Unregister(c <-chan T) {
 	s.Lock()
+	defer s.Unlock()
+
 	for i, v := range s.clients {
 		if v == c {
-			close(v)
 			// overwrite and chop method (most efficient use of memory)
 			s.clients[i] = s.clients[len(s.clients)-1] // copy the last to the removed
 			s.clients = s.clients[:len(s.clients)-1]   // shorten the slice
 			break
 		}
 	}
-	s.Unlock()
 }
 
 // Size returns the number of registered reception channels in the PubSub struct.
 func (s *PubSub[T]) Size() int {
+	s.RLock()
+	defer s.RUnlock()
 	return len(s.clients)
 }
 
 // IsActive returns true if there are active clients subscribed to the PubSub object, otherwise false.
 func (s *PubSub[T]) IsActive() bool {
+	s.RLock()
+	defer s.RUnlock()
 	return len(s.clients) > 0
 }
